6725ce517dcc47f8c0c66ff5feda44d56c01d1f0
mskora <michalvichy@gmail.com>
2015-05-16 10:32:17 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ngStopwatchApp
+ * @description
+ * # ngStopwatchApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ngStopwatchApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngStopwatchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngStopwatchApp
+ */
+angular.module('ngStopwatchApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
